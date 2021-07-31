f6929dc97ec4a9449066b492b6bed21fb09f212e
SyHoMadara <sh.sarafrazi80@gmail.com>
2021-07-31 23:24:55 +0430

    Google Test moved

---
+++ google_testing/AbsoluteDateTest.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+
+
+TEST(AbsoluteDateTestSuite, ExampleDate){
+
+//    EXPECT_EQ(gregDate.getAbsoluteDate(),737761);
+}
+
+
+TEST(AbsoluteDateTestSuite, IncorrectDate){ // 12/0/2020 -> 0
+//    ASSERT_EQ(1,1);
+//    ASSERT_EQ(gregDate.getAbsoluteDate(),0);
+}
